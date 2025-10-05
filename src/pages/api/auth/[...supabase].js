8226630e9158fcd8842096210e335abcb216b6b0
frodrigobapps <frodrigobapps@gmail.com>
2025-10-05 23:18:38 +0200

    Delete src/pages/api/auth/[...supabase].js

--- src/pages/api/auth/[...supabase].js
+++
@@ -1,6 +0,0 @@
-import { createPagesServerClient } from '@supabase/auth-helpers-nextjs';
-
-export default async function handler(req, res) {
-  const supabase = createPagesServerClient({ req, res });
-  await supabase.auth.api.setAuthCookie(req, res);
-}
